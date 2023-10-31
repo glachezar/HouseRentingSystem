d56a499ceb8af89cc0082afe62450b8b80a82656
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2023-10-31 10:14:47 +0200

    Added data seeding migration and House controller

---
+++ HouseRentingSystem/Controllers/HouseController.cs
@@ -0,0 +1,17 @@
+﻿namespace HouseRentingSystem.Web.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize]
+    public class HouseController : Controller
+    {
+
+        [AllowAnonymous]
+        public async Task<IActionResult> All()
+        {
+            return View();
+        }
+
+    }
+}
