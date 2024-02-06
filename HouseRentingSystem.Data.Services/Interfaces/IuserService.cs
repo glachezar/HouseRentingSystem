834f4ae0549a17eab9296f94beac2ef80524d234
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-02-06 11:32:15 +0200

    added user service interface

---
+++ HouseRentingSystem.Data.Services/Interfaces/IuserService.cs
@@ -0,0 +1,12 @@
+﻿namespace HouseRentingSystem.Data.Services.Interfaces;
+
+using Web.ViewModels.User;
+
+public interface IUserService
+{
+    Task<string> GetFullNameByEmailAsync(string email);
+
+    Task<string> GetFullNameByIdAsync(string userId);
+
+    Task<IEnumerable<UserViewModel>> AllAsync();
+}
