4bce5f017d54eeb7f953649b691a34d56dce5489
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-02-06 11:34:57 +0200

    Category service updated

--- HouseRentingSystem.Data.Services/CategoryService.cs
+++ HouseRentingSystem.Data.Services/CategoryService.cs
@@ -1,53 +1,81 @@
-﻿using HouseRentingSystem.Data.Services.Interfaces;
-using HouseRentingSystem.Web.ViewModels.Category;
+﻿namespace HouseRentingSystem.Data.Services;
+
 using Microsoft.EntityFrameworkCore;
 
-namespace HouseRentingSystem.Data.Services
+using Data;
+using Data.Models;
+using Interfaces;
+using Web.ViewModels.Category;
+
+public class CategoryService : ICategoryService
 {
-    
+    private readonly HouseRentingDbContext dbContext;
 
-    public class CategoryService : ICategoryService
+    public CategoryService(HouseRentingDbContext dbContext)
     {
-        private readonly HouseRentingDbContext dbContext;
+        this.dbContext = dbContext;
+    }
 
-        public CategoryService(HouseRentingDbContext dbContext)
+    public async Task<IEnumerable<HouseSelectCategoryFormModel>> AllCategoriesAsync()
+    {
+        IEnumerable<HouseSelectCategoryFormModel> allCategories = await dbContext
+            .Categories
+            .AsNoTracking()
+            .Select(c => new HouseSelectCategoryFormModel()
+            {
+                Id = c.Id,
+                Name = c.Name,
+            })
+            .ToArrayAsync();
+
+        return allCategories;
+    }
+
+    public async Task<IEnumerable<AllCategoriesViewModel>> AllCategoriesForListAsync()
+    {
+        IEnumerable<AllCategoriesViewModel> allCategories = await dbContext
+            .Categories
+            .AsNoTracking()
+            .Select(c => new AllCategoriesViewModel()
+            {
+                Id = c.Id,
+                Name = c.Name,
+            })
+            .ToArrayAsync();
+
+        return allCategories;
+    }
+
+    public async Task<bool> ExistsByIdAsync(int id)
+    {
+        bool result = await dbContext
+            .Categories
+            .AnyAsync(c => c.Id == id);
+
+        return result;
+    }
+
+    public async Task<IEnumerable<string>> AllCategoryNamesAsync()
+    {
+        IEnumerable<string> allNames = await dbContext
+            .Categories
+            .Select(c => c.Name)
+            .ToArrayAsync();
+
+        return allNames;
+    }
+
+    public async Task<CategoryDetailsViewModel> GetDetailsByIdAsync(int id)
+    {
+        Category category = await dbContext
+            .Categories
+            .FirstAsync(c => c.Id == id);
+
+        CategoryDetailsViewModel viewModel = new CategoryDetailsViewModel()
         {
-            this.dbContext = dbContext;
-        }
-
-        public async Task<IEnumerable<HouseSelectCategoryFormModel>> AllCategoriesAsync()
-        {
-            IEnumerable<HouseSelectCategoryFormModel> allCategories = await this.dbContext
-                    .Categories
-                    .AsNoTracking()
-                    .Select(c => new HouseSelectCategoryFormModel()
-                    {
-                        Id = c.Id,
-                        Name = c.Name,
-                    })
-                    .ToArrayAsync();
-
-            return allCategories;
-        }
-
-        public async Task<bool> CategoryExistByIdAsync(int id)
-        {
-            bool result = await dbContext
-                .Categories
-                .AnyAsync(c => c.Id == id);
-
-            return result;
-        }
-
-        public async Task<IEnumerable<string>> AllCategoryNamesAsync()
-        {
-            IEnumerable<string> allNames = await this.dbContext
-                .Categories
-                .Select(c => c.Name)
-                .ToArrayAsync();
-
-
-            return allNames;
-        }
+            Id = category.Id,
+            Name = category.Name
+        };
+        return viewModel;
     }
 }
